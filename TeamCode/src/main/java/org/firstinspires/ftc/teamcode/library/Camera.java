0815bf8939018bde9d0d856f912196c495a099a8
Hal Hockersmith <hal.hockersmith@gmail.com>
2019-10-21 19:24:11 -0500

    Changes to get camera working.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/Camera.java
@@ -1,18 +1,19 @@
 package org.firstinspires.ftc.teamcode.library;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 /**
  * Created by Hal on 11/14/2017.
  */
 
 public interface Camera {
-    public enum Glyph{
-        LEFT, RIGHT, CENTER, UNKNOWN
-    }
+    public void init(HardwareMap hardwareMap);
+    public boolean scan(int timeout);
+    public void setTelemetry(Telemetry telemetry);
 
-    public Glyph getGlyph();
-    public Location getLocation();
-
-    public Long getRawX();
-    public Long getRawY();
-    public Double getRawAngle();
+    public Float getX();
+    public Float getY();
+    public Float getAngle();
 }
