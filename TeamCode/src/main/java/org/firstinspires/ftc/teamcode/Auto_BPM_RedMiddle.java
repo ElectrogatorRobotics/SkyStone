fc1f096a0ffbcd3d5bbfa0111490cc5865a5e118
Mira <mira.eschliman@outlook.com>
2020-01-19 16:21:30 -0600

    stuff done at neola iowa

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_BPM_RedMiddle.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.library.Arm;
+import org.firstinspires.ftc.teamcode.library.ArmImpl;
+import org.firstinspires.ftc.teamcode.library.Drive;
+import org.firstinspires.ftc.teamcode.library.DriveImpl;
+
+@Autonomous(name="BPM - Red Middle", group="BuildPlate")
+public class Auto_BPM_RedMiddle extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Drive drive = new DriveImpl();
+        drive.initDrive(hardwareMap);
+        drive.init_bno055IMU(hardwareMap);
+        drive.passLinearOp(this);
+        drive.setTelemetry(telemetry);
+
+//        Camera camera = new CameraImpl();
+//        camera.init(hardwareMap);
+//        camera.setTelemetry(telemetry);
+
+        Arm arm = new ArmImpl(hardwareMap);
+
+        waitForStart();
+//        while(opModeIsActive()){
+                drive.slide(48);
+              drive.forward(4);
+              arm.grabFD();
+              drive.forward(-3);
+              drive.slide(-30);
+
+//            camera.scan(3000);
+//            telemetry.addData("Cam X:",camera.getX());
+//            telemetry.addData("Cam Y",camera.getY());
+//            telemetry.addData("Cam Ang",camera.getAngle());
+//            telemetry.update();
+//            sleep(2000);
+//        }
+    }
+}
